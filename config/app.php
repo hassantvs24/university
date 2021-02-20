6d7a78dcbcaff5dedb448e7ffd8e2f2ba787a125
hassantvs24 <hassantvs24@gmail.com>
2021-02-20 16:11:30 +0600

    Permission Update

--- config/app.php
+++ config/app.php
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
+
+        Spatie\Permission\PermissionServiceProvider::class, //User Authorization package
 
     ],
 
