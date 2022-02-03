ee08be1809bd376cc3fb9eab38c0f1ccafc4e9c2
= <spurkayastha83@gmail.com>
2022-02-03 17:00:00 +0600

    Multi step done without validation

---
+++ public/assets/js/pages/custom/form/multi-step-from.js
@@ -0,0 +1,67 @@
+var currentTab = 0;
+document.addEventListener("DOMContentLoaded", function (event) {
+    showTab(currentTab);
+});
+
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == x.length - 1) {
+        document.getElementById("nextBtn").style.display = "none";
+        document.getElementById("submitBtn").style.display = "inline";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        document.getElementById("nextprevious").style.display = "none";
+        document.getElementById("all-steps").style.display = "none";
+        document.getElementById("register").style.display = "none";
+        document.getElementById("text-message").style.display = "block";
+    }
+    showTab(currentTab);
+}
+
+function validateForm() {
+    var x,
+        y,
+        i,
+        valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    for (i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            y[i].className += " invalid";
+            valid = false;
+        }
+    }
+    if (valid) {
+        // var check_text = " <div class='check fas fa-check'></div>"
+        document.getElementsByClassName("step")[currentTab].className +=
+            " finish";
+        document.getElementsByClassName("step")[currentTab].innerHTML =
+            " ";
+        // document.getElementsByClassName("step")[currentTab].innerHTML = check_text;
+    }
+    return valid;
+}
+function fixStepIndicator(n) {
+    var i,
+        x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    x[n].className += " active";
+}
--- resources/views/student/student_create.blade.php
+++ resources/views/student/student_create.blade.php
@@ -10,11 +10,294 @@
         <div class="col">
             <x-card label="{{__('New Student Registration')}}">
 
+                <form id="studenRegistrationForm" action="{{route('student.store')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="institute" value="University" />
+                    <div class="all-steps" id="all-steps">
+                        <span class="step">1</span>
+                        <span class="step">2</span>
+                        <span class="step">3</span>
+                        <span class="step">4</span>
+                        <span class="step">5</span>
+                        <span class="step">6</span>
+                        <span class="step">7</span>
+                        <span class="step">8</span>
+                        <span class="step">9</span>
+                        <span class="step">10</span>
+                    </div>
+
+
+                    <div class="tab">
+                        <p class="bg-primary text-white"><i class="flaticon2-fast-next text-white"></i> <b>{{__('Primary Info')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Student Name')}}" name="name" required="required" />
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Student Email')}}" type="email" name="email" required="required" />
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Student Dob')}}" type="date" name="dob" required="required" />
+                        </div>
+                    </div>
+                    <!--Basic Info: End-->
+                    </div>
+                    <div class="tab">
+                        <!--General Info: Start-->
+                    <p class="bg-primary text-white"><i class="flaticon2-fast-next text-white"></i> <b>{{__('General Info')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Student Name (Bangla)')}}" name="bn_name" required="required" />
+                            <x-nselect label="{{__('Gender')}}" name="gender" required="required" >
+                                <option value="Male">{{__('Male')}}</option>
+                                <option value="Female">{{__('Female')}}</option>
+                                <option value="Not Specified">{{__('Not Specified')}}</option>
+                            </x-nselect>
+                            <x-ninput label="{{__('Nationality')}}" name="nationality"/>
+                        </div>
+
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Contact Number')}}" name="contact" required="required" />
+                            <x-nselect label="{{__('Marital Status')}}" name="marital_status" required="required" >
+                                <option value="Single">{{__('Single')}}</option>
+                                <option value="Married">{{__('Married')}}</option>
+                            </x-nselect>
+
+                            <div class="form-group">
+                                <label>{{__('NID')}}/{{__('Birth Certificate')}}/{{__('Passport')}}</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                      <span class="input-group-text">
+                                           <select name="id_type">
+                                               <option value="Nid">{{__('Nid')}}</option>
+                                               <option value="Bc">{{__('Birth Certificate')}}</option>
+                                               <option value="Passport">{{__('Passport')}}</option>
+                                           </select>
+                                        <span></span>
+                                       </label>
+                                      </span>
+                                    </div>
+                                    <input type="text" name="id_number" class="form-control" placeholder="{{__('ID Number')}}"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Birth Place')}}" name="birth_place"/>
+                            <x-nselect label="{{__('Blood Group')}}" name="blood_group" required="required" >
+                                <option value="A+">{{__('A+')}}</option>
+                                <option value="A-">{{__('A-')}}</option>
+                                <option value="B+">{{__('B+')}}</option>
+                                <option value="B-">{{__('B-')}}</option>
+                                <option value="O+">{{__('O+')}}</option>
+                                <option value="O-">{{__('O-')}}</option>
+                                <option value="AB+">{{__('AB+')}}</option>
+                                <option value="AB-">{{__('AB-')}}</option>
+                            </x-nselect>
+                            <x-ninput label="{{__('Signature Scan')}}" type="file" name="signature" accept="image/*" />
+                        </div>
+                    </div>
+                    <!--General Info: End-->
+                    </div>
+                    <div class="tab">
+                        <!--Present Address: Start-->
+                    <p class="bg-warning text-white"><i class="flaticon-home text-white"></i> <b>{{__('Present Address')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('House')}}" name="house"/>
+                            <x-ninput label="{{__('Post Office')}}" name="po"/>
+                            <x-ninput label="{{__('District')}}" name="district"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Road')}}" name="road"/>
+                            <x-ninput label="{{__('Post Code')}}" name="pc"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Village/Town')}}" name="village"/>
+                            <x-ninput label="{{__('Upazilla')}}" name="upazilla"/>
+
+                        </div>
+                    </div>
+                    <!--Present Address: End-->
+                    </div>
+                    <div class="tab">
+                        <p class="bg-warning text-white"><i class="flaticon-home-2 text-white"></i> <b>{{__('Permanent Address')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('House')}}" name="permanent_house"/>
+                            <x-ninput label="{{__('Post Office')}}" name="permanent_po"/>
+                            <x-ninput label="{{__('District')}}" name="permanent_district"/>
+
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Road')}}" name="permanent_road"/>
+                            <x-ninput label="{{__('Post Code')}}" name="permanent_pc"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Village/Town')}}" name="permanent_village"/>
+                            <x-ninput label="{{__('Upazilla')}}" name="permanent_upazilla"/>
+
+                        </div>
+                    </div>
+                    <!--Permanent Address: End-->
+                    </div>
+                    <div class="tab">
+                        <p class="bg-success text-white"><i class="flaticon2-avatar text-white"></i> <b>{{__('Father Info')}}</b></p>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <x-ninput label="{{__('Name')}}" name="father_name" required="required"/>
+                                <x-ninput label="{{__('Email')}}" type="email" name="father_email"/>
+                                <x-ninput label="{{__('NID')}}" name="father_nid"/>
+                            </div>
+                            <div class="col-md-4">
+                                <x-ninput label="{{__('Mobile')}}" name="father_contact" required="required"/>
+                                <x-ninput label="{{__('Organization')}}" name="father_organization"/>
+                                <x-ninput label="{{__('Photo')}}" type="file" name="father_photo" accept="image/*" />
+                            </div>
+                            <div class="col-md-4">
+                                <x-ninput label="{{__('Occupation')}}" name="father_occupation"/>
+                                <x-ninput label="{{__('Address')}}" name="father_address"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <p class="bg-success text-white"><i class="flaticon2-avatar text-white"></i> <b>{{__('Mother Info')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Name')}}" name="mother_name" required="required"/>
+                            <x-ninput label="{{__('Email')}}" type="email" name="mother_email"/>
+                            <x-ninput label="{{__('NID')}}" name="mother_nid"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Mobile')}}" name="mother_contact"/>
+                            <x-ninput label="{{__('Organization')}}" name="mother_organization"/>
+                            <x-ninput label="{{__('Photo')}}" type="file" name="mother_photo" accept="image/*" />
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Occupation')}}" name="mother_occupation"/>
+                            <x-ninput label="{{__('Address')}}" name="mother_address"/>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="tab">
+                        <p class="bg-success text-white"><i class="flaticon2-avatar text-white"></i> <b>{{__('Spouse Info')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Name')}}" name="spouse_name"/>
+                            <x-ninput label="{{__('Email')}}" type="email" name="spouse_email"/>
+                            <x-ninput label="{{__('NID')}}" name="spouse_nid"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Mobile')}}" name="spouse_contact"/>
+                            <x-ninput label="{{__('Organization')}}" name="spouse_organization"/>
+                            <x-ninput label="{{__('Photo')}}" type="file" name="spouse_photo" accept="image/*" />
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Occupation')}}" name="spouse_occupation"/>
+                            <x-ninput label="{{__('Address')}}" name="spouse_address"/>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="tab">
+                        <p class="bg-success text-white"><i class="flaticon2-avatar text-white"></i> <b>{{__('Local Guardian Info')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Name')}}" name="local_name"/>
+                            <x-ninput label="{{__('Email')}}" type="email" name="local_email"/>
+                            <x-ninput label="{{__('NID')}}" name="local_nid"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Mobile')}}" name="local_contact"/>
+                            <x-ninput label="{{__('Organization')}}" name="local_organization"/>
+                            <x-ninput label="{{__('Photo')}}" type="file" name="local_photo" accept="image/*" />
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Occupation')}}" name="local_occupation"/>
+                            <x-ninput label="{{__('Address')}}" name="local_address"/>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="tab">
+                        <p class="bg-success text-white"><i class="flaticon2-avatar text-white"></i> <b>{{__(' Person who will pay the fees')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Name')}}" name="payer_name"/>
+                            <x-ninput label="{{__('Email')}}" type="email" name="payer_email"/>
+                            <x-ninput label="{{__('NID')}}" name="payer_nid"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Mobile')}}" name="payer_contact"/>
+                            <x-ninput label="{{__('Organization')}}" name="payer_organization"/>
+                            <x-ninput label="{{__('Annual Income')}}" type="number" min="0" name="annual_income"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Occupation')}}" name="payer_occupation"/>
+                            <x-ninput label="{{__('Address')}}" name="payer_address"/>
+                            <x-ninput label="{{__('Photo')}}" type="file" name="payer_photo" accept="image/*" />
+                        </div>
+                    </div>
+                    <!--Guardian Info: End-->
+                    </div>
+                    <!--Other Info: Start-->
+                    <div class="tab">
+                        <p class="bg-primary text-white"><i class="flaticon2-fast-next text-white"></i> <b>{{__('Other Information')}}</b></p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Sibling/Spouse Name')}}" name="rtm_student_name"/>
+                            <x-ninput label="{{__('Sibling/Spouse ID')}}" name="rtm_student_id"/>
+                            <x-ninput label="{{__('SAT / TOFEL / IELTS / GMAT Score')}}" name="spoken_score"/>
+                            <x-ninput label="{{__('SAT / TOFEL / IELTS / GMAT Date Taken')}}" type="date" name="spoken_certificate_date"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-ninput label="{{__('Corporate Partner Name')}}" name="rtm_staff_name"/>
+                            <x-ninput label="{{__('Corporate Partner ID')}}" name="rtm_staff_id"/>
+                            <x-ninput label="{{__('Skilled in sports and others')}}" name="extra_activity"/>
+                        </div>
+                        <div class="col-md-4">
+                            <x-nselect label="{{__('Know About RTM')}}" name="know_about" >
+                                <option value="Staff">{{__('Staff')}}</option>
+                                <option value="Student">{{__('Student')}}</option>
+                                <option value="Friends">{{__('Friends')}}</option>
+                                <option value="Newspaper">{{__('Newspaper')}}</option>
+                                <option value="Social Medea">{{__('Social Medea')}}</option>
+                                <option value="SMS">{{__('SMS')}}</option>
+                                <option value="ADS">{{__('ADS')}}</option>
+                                <option value="Other Source">{{__('Other Source')}}</option>
+                            </x-nselect>
+
+                            <x-nselect label="{{__('Have Any dismissed/suspended/expelled?')}}" id="expelled" name="is_expelled" >
+                                <option value="0">{{__('No')}}</option>
+                                <option value="1">{{__('Yes')}}</option>
+                            </x-nselect>
+
+                            <div class="form-group" id="reason" style="display: none;">
+                                <label for="reasonx">{{__('Reason for dismissed/suspended/expelled')}}</label>
+                                <input id="reasonx" name="expelled_reason" type="text" class="form-control" value="{{old('expelled_reason') ?? ''}}" placeholder="{{__('Reason')}}"/>
+                            </div>
+                            <x-ninput label="{{__('Description')}}" name="description"/>
+                        </div>
+                    </div>
+                    <!--Other Info: End-->
+                    </div>
+
+                    <div style="overflow:auto;" id="nextprevious">
+                        <div style="float:right;">
+                            <button class="btn btn-warning" type="button" id="prevBtn" onclick="nextPrev(-1)"><i class="fa fa-arrow-alt-circle-left"></i> Previous</button>
+                            <button class="btn btn-success" type="button" id="nextBtn" onclick="nextPrev(1)">Next <i class="fa fa-arrow-alt-circle-right"></i></button>
+
+                            <button class="btn btn-success" type="submit" id="submitBtn">Save  <i class="fa fa-check"></i></button>
+                        </div>
+                    </div>
+
+                </form>
 
 
 
 
-{{--                <form action="{{route('student.store')}}" method="post" enctype="multipart/form-data">
+
+
+
+               {{-- <form action="{{route('student.store')}}" method="post" enctype="multipart/form-data">
                     @csrf
                     <input type="hidden" name="institute" value="University" />
                     <p class="bg-primary text-white"><i class="flaticon2-fast-next text-white"></i> <b>{{__('Primary Info')}}</b></p>
@@ -275,7 +558,7 @@
                         </div>
                     </div>
 
-                </form>--}}
+                </form> --}}
 
             </x-card>
         </div>
@@ -284,11 +567,13 @@
 @endsection
 
 @section('style')
+   <link href="{{asset('assets/css/pages/form/multistep-form.css')}}" rel="stylesheet" type="text/css" />
 {{--    <link href="{{asset('assets/css/pages/wizard/wizard-2.css')}}" rel="stylesheet" type="text/css" />--}}
 @endsection
 
 
 @section('script')
+   <script src="{{asset('assets/js/pages/custom/form/multi-step-from.js')}}"></script>
 {{--    <script src="{{asset('assets/js/pages/custom/wizard/wizard-2.js')}}"></script>--}}
     <script type="text/javascript">
         $(function () {
